2ab890a28725aa80a05f99d9d3cd948597c21895
Butryn Vasiliy <butrin.vasiliy@gmail.com>
2022-08-01 17:45:44 +0300

    Task1 commit

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,10 @@
 public class Task1 {
     public static void main(String[] args) {
-
-    }
+   int a = 9;
+   int b = 3;
+   System.out.println("Сума = "+(a+b));
+   System.out.println("Різниця = "+(a-b));
+   System.out.println("Добуток = "+(a*b));
+   System.out.println("Частка = "+(a/b));
+      }
 }
